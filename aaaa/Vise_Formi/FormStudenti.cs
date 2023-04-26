9fcf76240b8f18930199cb189da866e2584c21da
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-04-26 21:25:58 +0200

    Add files via upload

---
+++ aaaa/Vise_Formi/FormStudenti.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vise_Formi
+{
+    public partial class FormStudenti : Form
+    {
+        public FormStudenti()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            FormUnosNovogStudenta frm = new FormUnosNovogStudenta();
+
+            DialogResult rez = frm.ShowDialog();
+
+            // unos novog studenta u listBox
+
+            if (rez == System.Windows.Forms.DialogResult.OK)
+
+                lbxStudenti.Items.Add(frm.Student1.ToString());
+        }
+
+        private void btnObrisiStudenta_Click(object sender, EventArgs e)
+        {
+            lbxStudenti.Items.RemoveAt(lbxStudenti.Items.Count -1);
+        }
+
+        private void btnObrisiSve_Click(object sender, EventArgs e)
+        {
+            lbxStudenti.Items.Clear();
+        }
+    }
+}
---
+++ aaaa/Vise_Formi/FormUnosNovogStudenta.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Vise_Formi
+{
+    partial class FormUnosNovogStudenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBrojIndeksa = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbxSmjer = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpDatumRodjenja = new System.Windows.Forms.DateTimePicker();
+            this.btnSnimi = new System.Windows.Forms.Button();
+            this.btnOtkazi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime:";
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(27, 29);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 20);
+            this.txtIme.TabIndex = 1;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(27, 80);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(100, 20);
+            this.txtPrezime.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prezime:";
+            // 
+            // txtBrojIndeksa
+            // 
+            this.txtBrojIndeksa.Location = new System.Drawing.Point(231, 29);
+            this.txtBrojIndeksa.Name = "txtBrojIndeksa";
+            this.txtBrojIndeksa.Size = new System.Drawing.Size(100, 20);
+            this.txtBrojIndeksa.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Broj Indeksa:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Smjer:";
+            // 
+            // cbxSmjer
+            // 
+            this.cbxSmjer.FormattingEnabled = true;
+            this.cbxSmjer.Items.AddRange(new object[] {
+            "Programer",
+            "Sistem administrator",
+            "Mrežni administrator"});
+            this.cbxSmjer.Location = new System.Drawing.Point(27, 132);
+            this.cbxSmjer.Name = "cbxSmjer";
+            this.cbxSmjer.Size = new System.Drawing.Size(121, 21);
+            this.cbxSmjer.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(228, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Datum rođenja:";
+            // 
+            // dtpDatumRodjenja
+            // 
+            this.dtpDatumRodjenja.Location = new System.Drawing.Point(231, 79);
+            this.dtpDatumRodjenja.Name = "dtpDatumRodjenja";
+            this.dtpDatumRodjenja.Size = new System.Drawing.Size(200, 20);
+            this.dtpDatumRodjenja.TabIndex = 9;
+            // 
+            // btnSnimi
+            // 
+            this.btnSnimi.Location = new System.Drawing.Point(27, 173);
+            this.btnSnimi.Name = "btnSnimi";
+            this.btnSnimi.Size = new System.Drawing.Size(75, 23);
+            this.btnSnimi.TabIndex = 10;
+            this.btnSnimi.Text = "Snimi";
+            this.btnSnimi.UseVisualStyleBackColor = true;
+            this.btnSnimi.Click += new System.EventHandler(this.btnSnimi_Click);
+            // 
+            // btnOtkazi
+            // 
+            this.btnOtkazi.Location = new System.Drawing.Point(356, 173);
+            this.btnOtkazi.Name = "btnOtkazi";
+            this.btnOtkazi.Size = new System.Drawing.Size(75, 23);
+            this.btnOtkazi.TabIndex = 11;
+            this.btnOtkazi.Text = "Otkazi";
+            this.btnOtkazi.UseVisualStyleBackColor = true;
+            this.btnOtkazi.Click += new System.EventHandler(this.btnOtkazi_Click);
+            // 
+            // FormUnosNovogStudenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOtkazi);
+            this.Controls.Add(this.btnSnimi);
+            this.Controls.Add(this.dtpDatumRodjenja);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbxSmjer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBrojIndeksa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.label1);
+            this.Name = "FormUnosNovogStudenta";
+            this.Text = "FormUnosNovogStudenta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBrojIndeksa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbxSmjer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpDatumRodjenja;
+        private System.Windows.Forms.Button btnSnimi;
+        private System.Windows.Forms.Button btnOtkazi;
+    }
+}
